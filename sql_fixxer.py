c7b80a53077303b6028e54188afdca8a77a4fa26
Zach Fudge <zfudge@spotx.tv>
2018-11-16 23:11:52 -0700

    Initial commit
     Changes to be committed:
    	new file:   sql_fixxer.py

---
+++ sql_fixxer.py
@@ -0,0 +1,129 @@
+import os
+import re
+
+# increment .sql files changed
+changes = 0
+
+fix = {
+	"keywords": {
+		"select": "SELECT",
+		"distinct": "DISTINCT",
+		"case": "CASE",
+		"when": "WHEN",
+		"then": "THEN",
+		"end": "END",
+		"else": "ELSE",
+		"from": "FROM",
+		"as": "AS",
+		"left": "LEFT",
+		"inner": "INNER",
+		"right": "RIGHT",
+		"join": "JOIN",
+		"union": "UNION",
+		"where": "WHERE",
+		"and": "AND",
+		"or": "OR",
+		"not": "NOT",
+		"on": "ON",
+		"in": "IN",
+		"is": "IS",
+		"if": "IF",
+		"using": "USING",
+		"like": "LIKE",
+		"group": "GROUP",
+		"order": "ORDER",
+		"by": "BY",
+		"desc": "DESC",
+		"asc": "ASC"
+	},
+	"substrings": {
+		"date_format": "DATE_FORMAT",
+		"coalesce": "COALESCE",
+		"curdate": "CURDATE",
+		"concat": "CONCAT",
+		"ifnull": "IFNULL",
+		"count": "COUNT",
+		"round": "ROUND",
+		"sum": "SUM",
+		"avg": "AVG",
+		"min": "MIN",
+		"max": "MAX",
+		"nvl": "NVL",
+		"if": "IF"
+	},
+	"operators": [
+		">=",
+		"<=",
+		"<>",
+		">",
+		"<",
+		"=",
+		"+",
+		"-",
+		"/",
+		"*"	
+	]
+}
+
+def check_indexes(index_list, operator_index):
+
+
+def check_operators(line):
+	for operator in fix["operators"]:
+		if operators in line:
+			single_indexes = re.finditer("'", line)
+			double_indexes = re.finditer('"', line)
+
+
+
+def sql_writer(file_name):
+	global changes
+	print file_name
+	sql_read = open(file_name, 'r')
+	sql = sql_read.read()
+	sql_read.close()
+	sql = sql.split("\n")
+	change = 0
+
+	for ind, line in enumerate(sql):
+		# extract leading whitespace
+		prepend = line[:-len(line.lstrip())]
+		
+		line = check_operators(line)
+		line = line.split()
+
+		for index, word in enumerate(line):
+			if word in fix["keywords"]:
+				line[index] = fix["keywords"][word]
+				change += 1
+			else:
+				word = word.split("(")
+				for i, w in enumerate(word):
+					for key, value in fix["substrings"].iteritems():
+						if key in w:
+							word[i] = w.replace(key, value)
+							change += 1
+				word = "(".join(word)
+				line[index] = word
+		
+		line = " ".join(line)
+		
+		if ("GROUP BY" in line or "ORDER BY" in line or "SELECT" in line) and line.count(",") > 1:
+			line = line.replace(",", ",\n   ")
+			if line[-5:] == ",\n   ": line = line[:-4] 
+		sql[ind] = prepend + line
+
+	sql = "\n".join(sql)
+
+	print "{} change{}".format(change, "s" if change is not 1 else "")
+	if change > 0: changes += 1
+
+	text_file = open(file_name, "w")
+	text_file.write(sql)
+	text_file.close()
+
+for file in os.listdir("."):
+    if file.endswith(".sql"): 
+        sql_writer(file)
+
+print "{} .sql files altered.".format(changes)
